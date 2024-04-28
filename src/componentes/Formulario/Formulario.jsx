cb06a6ae9dd0fd60fda9520dcadfed1b7a595278
Fernanda <fernanda.rmonroy@gmail.com>
2024-04-28 14:19:22 -0400

    cambio css en formulario, registro and social button

--- src/componentes/Formulario/Formulario.jsx
+++ src/componentes/Formulario/Formulario.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useState } from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 function Formulario({ addAlert }) {
   const [inputs, setInputs] = useState({
-    nombre: '',
-    email: '',
-    password: '',
-    password1: '',
+    nombre: "",
+    email: "",
+    password: "",
+    password1: "",
   });
 
   function inputsHandler(e) {
-    if (e.target.id === 'nombre') {
+    if (e.target.id === "nombre") {
       setInputs({ ...inputs, nombre: e.target.value });
     }
 
-    if (e.target.id === 'email') {
+    if (e.target.id === "email") {
       setInputs({ ...inputs, email: e.target.value });
     }
 
-    if (e.target.id === 'password') {
+    if (e.target.id === "password") {
       setInputs({ ...inputs, password: e.target.value });
     }
-    if (e.target.id === 'password1') {
+    if (e.target.id === "password1") {
       setInputs({ ...inputs, password1: e.target.value });
     }
   }
@@ -33,43 +33,50 @@ function Formulario({ addAlert }) {
 
     const isValidNombre = /^[a-zA-Z0-9]{4,}$/;
     const isValidEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const isValidPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
+    const isValidPassword =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
 
-
-    if (inputs.nombre.trim() === '' || inputs.email === '' || inputs.password === '' || inputs.password1 === '') {
+    if (
+      inputs.nombre.trim() === "" ||
+      inputs.email === "" ||
+      inputs.password === "" ||
+      inputs.password1 === ""
+    ) {
       addAlert({
-        texto: 'Debes completar todos los campos!',
-        tipo: 'alert-danger',
+        texto: "Debes completar todos los campos!",
+        tipo: "alert-danger",
         estado: true,
       });
     } else if (!isValidNombre.test(inputs.nombre)) {
-        addAlert({
-          texto: 'El Nombre debe tener minimo 4 caracteres y no se permiten caracteres especiales!',
-          tipo: 'alert-danger',
-          estado: true,
-        });
+      addAlert({
+        texto:
+          "El Nombre debe tener minimo 4 caracteres y no se permiten caracteres especiales!",
+        tipo: "alert-danger",
+        estado: true,
+      });
     } else if (!isValidEmail.test(inputs.email)) {
       addAlert({
-        texto: 'Formato de email incorrecto!',
-        tipo: 'alert-danger',
+        texto: "Formato de email incorrecto!",
+        tipo: "alert-danger",
         estado: true,
       });
     } else if (!isValidPassword.test(inputs.password)) {
       addAlert({
-        texto: 'La contraseña debe tener minimo 8 caracteres, una letra mayúscula, una letra minúscula y un número!',
-        tipo: 'alert-danger',
+        texto:
+          "La contraseña debe tener minimo 8 caracteres, una letra mayúscula, una letra minúscula y un número!",
+        tipo: "alert-danger",
         estado: true,
       });
     } else if (inputs.password != inputs.password1) {
       addAlert({
-        texto: 'Ambas contraseñas deben ser iguales!',
-        tipo: 'alert-danger',
+        texto: "Ambas contraseñas deben ser iguales!",
+        tipo: "alert-danger",
         estado: true,
       });
     } else {
       addAlert({
-        texto: 'Registro creado exitosamente!',
-        tipo: 'alert-success',
+        texto: "Registro creado exitosamente!",
+        tipo: "alert-success",
         estado: true,
       });
     }
@@ -94,7 +101,7 @@ function Formulario({ addAlert }) {
             id="email"
             name="Email"
             type="email"
-            placeholder="tuemail@ejemplo.com"
+            placeholder="email@ejemplo.com"
           />
         </Form.Group>
 
--- src/componentes/Registro/Registro.jsx
+++ src/componentes/Registro/Registro.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import SocialButton from '../SocialButton/SocialButton';
-import Formulario from '../Formulario/Formulario.jsx';
+import React from "react";
+import SocialButton from "../SocialButton/SocialButton";
+import Formulario from "../Formulario/Formulario.jsx";
 
 export default function Registro({ onAlert }) {
-  
   return (
     <>
       <h1>Crea una cuenta</h1>
-      <SocialButton button={'fa-facebook'} />
-      <SocialButton button={'fa-github'} />
-      <SocialButton button={'fa-linkedin'} />
-      <p>O usa tu email para registrarte.</p>
+      <SocialButton button={"fa-facebook"} />
+      <SocialButton button={"fa-github"} />
+      <SocialButton button={"fa-linkedin"} />
+      <p>O regístrarte con tu mail.</p>
       <Formulario addAlert={onAlert} />
     </>
   );
