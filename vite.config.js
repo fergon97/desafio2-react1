81136db1b18d7d7dec560d6acbfb9ac4db08493b
Fernanda <fernanda.rmonroy@gmail.com>
2024-04-28 13:53:21 -0400

    gh-pages and collaboration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/desafio2-react1",
   plugins: [react()],
-  base: '/desafio2-formulario'
-})
+});
